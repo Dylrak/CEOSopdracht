96bf7055b580eacf3c7ee7cbaf5274779b59e156
dylrak <dylrak@hotmail.com>
2017-03-14 22:06:13 +0100

    Added support for three bit bus and flipflop input & output. The three bit bus is on BCM 17, 27 and 22, representing bits 4, 2 and 1 respectively. The flipflop input is on BCM 23 and the output on BCM 24.

--- CEOSopdracht/CEOS/main.c
+++ CEOSopdracht/CEOS/main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <wiringPi.h>
 
-int get_bit(unsigned char ch, int n) {//Index is 1-based, not zero
-	n = 8 - n;
+const char three_bit_bus[] = {0, 2, 3}; //BCM 17, 27 and 22 for a 3-bit parallel three_bit_bus we use to send a number to the board. 4, 2, 1 in order.
+const char FF_input = 4; //BCM 23 for flipflop input.
+const char FF_output = 5; //BCM 24 for flipflop output to reset.
+int score = 0;
+
+int get_bit(unsigned char ch, int n) {//Index is 1-based, not zero. n=0 is right-most bit.
 	unsigned char mask=1<<n;
 	if ((ch & mask)>>n) {
 		return HIGH;
@@ -10,20 +14,44 @@ int get_bit(unsigned char ch, int n) {//Index is 1-based, not zero
 		return LOW;
 	}
 }
+
+int send_bits(char current_number) { //Current number represents what lights will be on.
+	if (!digitalRead(FF_input)) { //On input from flipflop we count what score the player has:
+		if (current_number > 4) { //If the current number is higher than the middle light we start counting backwards.
+			score += currentnumber - 2 * (currentnumber - 4); //So light 5 = score 3, 6 = 2, 7 = 1.
+		} else { //Else, the score is our current number.
+			score += current_number; 
+		}
+		printf("Score is: %d", score); //Print it,
+		digitalWrite(FF_output, HIGH); //And reset the flipflop.
+		delay(1000);
+		digitalWrite(FF_output, LOW);
+	} else { //Otherwise we send the bits of our current number down the three bit bus.
+		for (int bit = 0; bit < 3; bit++) {
+			digitalWrite (three_bit_bus[bit], get_bit(current_number, bit));
+		}
+	}
+}
+
 int main(int argc, char **argv)
 {
 	wiringPiSetup();
-	pinMode(5, OUTPUT); //4
-	pinMode(6, OUTPUT); //2
-	pinMode(13, OUTPUT);//1. We use 5,6 and 13 as parallel bus.
-	pinMode(19, INPUT);
-	pinMode(26, OUTPUT);
-	
-	unsigned char current_number = 1;
+	for (int bit = 0; bit < 3; bit++) {
+		pinMode(three_bit_bus[bit], OUTPUT);
+	}
+	pinMode(FF_input, INPUT);
+	pinMode(FF_output, OUTPUT);
 
-	for (int i = 1; i < 5; i++) {
-		printf("%d", get_bit(current_number, 0));
-		digitalWrite (13, get_bit(current_number, 0));
+	printf("Game start!");
+	while(1) {
+		for (char i = 1; i < 8; i++) {
+			send_bits(i);
+			delay(100);
+		}
+		for (char i = 7; i > 0; i--) {
+			send_bits(i);
+			delay(100);
+		}
 	}
 	return 0;
 }

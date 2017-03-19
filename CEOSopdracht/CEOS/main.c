ef8d348f623c83c009ef435bc9ea7bb1e7aae0fb
dylrak <dylrak@hotmail.com>
2017-03-19 20:17:29 +0100

    code cleanup

--- CEOSopdracht/CEOS/main.c
+++ CEOSopdracht/CEOS/main.c
@@ -4,8 +4,8 @@
 const char three_bit_bus[] = {0, 2, 3}; //BCM 17, 27 and 22 for a 3-bit parallel three_bit_bus we use to send a number to the board. 4, 2, 1 in order.
 const char FF_input = 4; //BCM 23 for flipflop input.
 const char FF_output = 5; //BCM 24 for flipflop output to reset.
-const int max_delay = 300;
-int score = 0;
+const int max_delay = 600;
+int total_score = 0;
 
 int get_bit(unsigned char ch, int n) {//n=0 is right-most bit.
 	unsigned char mask=1<<n;
@@ -27,18 +27,18 @@ void flash_light(char current_light) {
 }
 
 int player_has_shot(char current_light) { //Returns true if we get input from the flipflop.
-	int current_delay; //We start by calculating the delay based on what light we are on. The closer to the centre, the smaller the delay.
+	int current_score;//We start by calculating the score based on what light we are on. The closer to the centre, the smaller the delay.
 	if (current_light > 4) { //If the current number is higher than the middle light we start counting backwards.
-		current_delay = max_delay / (current_light - 2 * (current_light - 4)); //So light 5 = score 3, 6 = 2, 7 = 1.
+		current_score = (current_light - 2 * (current_light - 4)); //So light 5 = score 3, 6 = 2, 7 = 1.
 	} else { //Else, the score is our current number.
-		current_delay = max_delay / current_light;
+		current_score = current_light;
 	}
-	delay(current_delay);
+	delay(max_delay / current_score); //Delay our input check based on our current score. If the score is 4, the highest, our delay is the smallest.
 	if (!digitalRead(FF_input)) { //On input from flipflop we count what score the player has:
-		score += max_delay / current_delay;
-		printf("Score is: %d\n", score); //Print it,
-		digitalWrite(FF_output, HIGH); //And reset the flipflop.
-		flash_light(current_light);
+		total_score += current_score;
+		printf("Current score is: %d\nTotal score is: %d\n", current_score, total_score);
+		digitalWrite(FF_output, HIGH); //Reset the flipflop.
+		flash_light(current_light); //Flash light in the meanwhile to indicate during which light you shot.
 		digitalWrite(FF_output, LOW);
 		return 1;
 	}
@@ -53,6 +53,7 @@ void send_bits(char current_light) { //Current_light is the bits representing wh
 
 int main(int argc, char **argv)
 {
+	//INIT
 	wiringPiSetup();
 	for (int bit = 0; bit < 3; bit++) {
 		pinMode(three_bit_bus[bit], OUTPUT);
@@ -65,14 +66,11 @@ int main(int argc, char **argv)
 		for (char i = 1; i < 8; i++) {
 			send_bits(i);
 			if(player_has_shot(i))
-				printf("Current score is: %d\n", score);
 				break;
 		}
 		for (char i = 7; i > 0; i--) {
 			send_bits(i);
 			if(player_has_shot(i))
-				printf("Current score is: %d\n", score);
-				delay(max_delay);
 				break;
 		}
 	}
